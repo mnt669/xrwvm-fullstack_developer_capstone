cec79c49ec9b2b3402578ea071f2c8d671ec8423
mnt669 <montassar579@gmail.com>
2024-12-03 10:48:06 +0100

    added user management-related templates and views to the app

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,10 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from "./components/Dealers/PostReview";
+
 
 function App() {
   return (
